39126deb9948978ae426a1cd8566748d0cabafdb
Jose Manuel Montano Mengual <jm3develop@gmail.com>
2025-02-11 15:04:51 +0100

    added basic styling

--- src/pages/public/ChatPage.tsx
+++ src/pages/public/ChatPage.tsx
@@ -1,5 +1,15 @@
 const ChatPage = () => {
-  return <div>Chat</div>;
+  return (
+    <div className="flex flex-col items-center py-12 gap-4">
+      <h1>Chat</h1>
+      <section>Chat response</section>
+      <section>
+        <form action=""></form>
+        <input type="text" className="border rounded-md" />
+        <button></button>
+      </section>
+    </div>
+  );
 };
 
 export default ChatPage;
