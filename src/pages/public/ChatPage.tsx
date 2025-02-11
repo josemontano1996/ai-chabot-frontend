decca600f23e39b94a7c6d959c856cdb83d0f0f5
Jose Manuel Montano Mengual <jm3develop@gmail.com>
2025-02-11 20:58:20 +0100

    created custom hook for handling websockets and added zustand for controlling state

--- src/pages/public/ChatPage.tsx
+++ src/pages/public/ChatPage.tsx
@@ -1,73 +1,32 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useCallback, useEffect, useRef, useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { z } from 'zod';
+import { ChatMessage, useChatStore } from '../../store/ChatStore';
+import { useWebSocket } from '@/hooks/useWebSocket';
+import { useAppStore } from '@/store/AppStore';
+import { useCallback } from 'react';
 
 const schema = z.object({
   message: z.string().min(1, 'Message is required.'),
 });
-
-interface WSMessage {
-  type: string;
-  message: string;
-}
-
-type History = WSMessage[];
 
 type FormFields = z.infer<typeof schema>;
 
 const wsAddress = 'ws://localhost:8080/ws';
 
 const ChatPage = () => {
-  const ws = useRef<WebSocket | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [history, setHistory] = useState<History>([]);
+  const chatMessages = useChatStore((state) => state.messages);
+  const isLoading = useAppStore((state) => state.isLoading);
+  const setIsLoading = useAppStore((state) => state.setIsLoading);
+  const addMessage = useChatStore((state) => state.addMessage);
 
-  const connectWebSocket = useCallback(() => {
-    // Function to establish and re-establish connection
-    ws.current = new WebSocket(wsAddress);
-
-    ws.current.onopen = () => {
-      console.log('connected to ws');
-    };
-
-    ws.current.onmessage = (event) => {
-      try {
-        const parsedMessage = JSON.parse(event.data);
-        setHistory((prevMessages) => [...prevMessages, parsedMessage]); // Access the message field
-      } catch (error) {
-        console.error('Error parsing message:', error, event.data);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    ws.current.onerror = (error) => {
-      console.error('WebSocket error:', error);
-    };
-
-    ws.current.onclose = (event) => {
-      console.log(
-        'Disconnected from WebSocket server',
-        event.code,
-        event.reason
-      );
-      ws.current = null;
-      if (!event.wasClean) {
-        console.log('Disconnnected, attempting to reconnect...');
-        setTimeout(connectWebSocket, 3000);
-      }
-    };
-  }, []);
-
-  useEffect(() => {
-    connectWebSocket();
-    return () => {
-      if (ws.current) {
-        ws.current.close();
-      }
-    };
-  }, [connectWebSocket]);
+  const onMessage = useCallback(
+    (msg: ChatMessage) => {
+      addMessage(msg);
+    },
+    [addMessage]
+  );
+  const { ws } = useWebSocket({ wsAddress, onMessage });
 
   const {
     register,
@@ -101,7 +60,7 @@ const ChatPage = () => {
       <section>
         <h2>Chat conversation</h2>
         <ul>
-          <li>{JSON.stringify(history).toString()}</li>
+          <li>{JSON.stringify(chatMessages).toString()}</li>
         </ul>
       </section>
       <section>
