3e91495185e1b3c7b933c9e16504c392ff05bd7d
Jose Manuel Montano Mengual <jm3develop@gmail.com>
2025-02-11 19:46:58 +0100

    ws is working

--- src/pages/public/ChatPage.tsx
+++ src/pages/public/ChatPage.tsx
@@ -1,4 +1,5 @@
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { z } from 'zod';
 
@@ -6,13 +7,72 @@ const schema = z.object({
   message: z.string().min(1, 'Message is required.'),
 });
 
+interface WSMessage {
+  type: string;
+  message: string;
+}
+
+type History = WSMessage[];
+
 type FormFields = z.infer<typeof schema>;
 
+const wsAddress = 'ws://localhost:8080/ws';
+
 const ChatPage = () => {
+  const ws = useRef<WebSocket | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [history, setHistory] = useState<History>([]);
+
+  const connectWebSocket = useCallback(() => {
+    // Function to establish and re-establish connection
+    ws.current = new WebSocket(wsAddress);
+
+    ws.current.onopen = () => {
+      console.log('connected to ws');
+    };
+
+    ws.current.onmessage = (event) => {
+      try {
+        const parsedMessage = JSON.parse(event.data);
+        setHistory((prevMessages) => [...prevMessages, parsedMessage]); // Access the message field
+      } catch (error) {
+        console.error('Error parsing message:', error, event.data);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    ws.current.onerror = (error) => {
+      console.error('WebSocket error:', error);
+    };
+
+    ws.current.onclose = (event) => {
+      console.log(
+        'Disconnected from WebSocket server',
+        event.code,
+        event.reason
+      );
+      ws.current = null;
+      if (!event.wasClean) {
+        console.log('Disconnnected, attempting to reconnect...');
+        setTimeout(connectWebSocket, 3000);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    connectWebSocket();
+    return () => {
+      if (ws.current) {
+        ws.current.close();
+      }
+    };
+  }, [connectWebSocket]);
+
   const {
     register,
     handleSubmit,
-    formState: { errors, isSubmitting },
+    formState: { errors },
     setError,
   } = useForm<FormFields>({
     resolver: zodResolver(schema),
@@ -20,9 +80,15 @@ const ChatPage = () => {
 
   const submitForm: SubmitHandler<FormFields> = async (data) => {
     try {
-      console.log(data);
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      throw new Error();
+      setIsLoading(true);
+      const payload = {
+        message: data.message,
+        type: 'user',
+      };
+      console.log(JSON.stringify(payload));
+      if (ws.current) {
+        ws.current.send(JSON.stringify(payload));
+      }
     } catch {
       setError('root', {
         message: 'An error ocurred',
@@ -35,8 +101,7 @@ const ChatPage = () => {
       <section>
         <h2>Chat conversation</h2>
         <ul>
-          <li>hoa</li>
-          <li>mesg2</li>
+          <li>{JSON.stringify(history).toString()}</li>
         </ul>
       </section>
       <section>
@@ -46,11 +111,11 @@ const ChatPage = () => {
             <span className="text-red-500">{errors.message.message}</span>
           )}
           <button
-            disabled={isSubmitting}
+            disabled={isLoading}
             type="submit"
             className="bg-green-600 px-2 py-1 rounded-md hover:cursor-pointer"
           >
-            {isSubmitting ? 'Loading' : 'Send'}
+            {isLoading ? 'Loading' : 'Send'}
           </button>
           {errors.root && (
             <span className="text-red-500">{errors.root.message}</span>
