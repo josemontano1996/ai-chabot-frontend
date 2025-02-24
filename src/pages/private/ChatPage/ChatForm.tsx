3c5f0b3ad9be45f805867c29fc9824e5067949be
Jose Manuel Montano Mengual <jm3develop@gmail.com>
2025-02-24 20:18:16 +0100

    improved chat forinput design, now it takes full width and it is correctly positioned

--- src/pages/private/ChatPage/ChatForm.tsx
+++ src/pages/private/ChatPage/ChatForm.tsx
@@ -1,24 +1,24 @@
-import { useWebSocketStore } from '@/store/WsStore';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
-import { z } from 'zod';
+import { useWebSocketStore } from "@/store/WsStore";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
   FormMessage,
-} from '@/components/shadcn/form';
-import { Input } from '@/components/shadcn/input';
-import { useAppStore } from '@/store/AppStore';
-import { TChatWebSocket } from '@/types/websockets';
-import { Button } from '@/components/shadcn/button';
-import { IChatMessage } from '@/types/chat';
-import { useChatMessagesStore } from '@/store/ChatMessagesStore';
-import { SendHorizonal } from 'lucide-react';
-import LoadingSpinner from '@/components/shared/LoadingSpinner';
+} from "@/components/shadcn/form";
+import { Input } from "@/components/shadcn/input";
+import { useAppStore } from "@/store/AppStore";
+import { TChatWebSocket } from "@/types/websockets";
+import { Button } from "@/components/shadcn/button";
+import { IChatMessage } from "@/types/chat";
+import { useChatMessagesStore } from "@/store/ChatMessagesStore";
+import { SendHorizonal } from "lucide-react";
+import LoadingSpinner from "@/components/shared/LoadingSpinner";
 const formSchema = z.object({
-  message: z.string().min(1, 'Message is required.'),
+  message: z.string().min(1, "Message is required."),
 });
 
 interface Props {
@@ -31,7 +31,7 @@ const ChatForm = ({ ws }: Props) => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      message: '',
+      message: "",
     },
   });
 
@@ -54,39 +54,41 @@ const ChatForm = ({ ws }: Props) => {
 
       // The setIsLoading state to false has to be handled by the ws.onmessage event, as the message is sent asynchronously.
     } catch {
-      setError('An error occurred. Please try again.');
+      setError("An error occurred. Please try again.");
     }
   }
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
-        <div className="flex items-center justify-between border rounded-full">
-          <FormField
-            control={form.control}
-            name="message"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Input
-                    placeholder={
-                      !isConnected
-                        ? 'Connecting to server...'
-                        : 'Type a message'
-                    }
-                    {...field}
-                    className="border-0 rounded-full  focus-visible:ring-0 focus-visible:ring-offset-0"
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+        <div className="flex items-center rounded-full border">
+          <div className="rounded-full mx-1 flex-grow">
+            <FormField
+              control={form.control}
+              name="message"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      placeholder={
+                        !isConnected
+                          ? "Connecting to server..."
+                          : "Type a message"
+                      }
+                      {...field}
+                      className="border-0 rounded-full focus-visible:ring-0 focus-visible:ring-offset-0"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
           <div>
             <Button
-              variant={'secondary'}
+              variant={"secondary"}
               type="submit"
               disabled={!isConnected || isLoading}
-              className="rounded-full m-1 p-3"
+              className="m-1 rounded-full p-3"
             >
               {!isConnected || isLoading ? (
                 <LoadingSpinner />
